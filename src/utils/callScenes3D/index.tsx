1459f7170ad50f9cd9c4414c1041b7688ed35ff5
Luis Murillo Baltodano <37167842+LuisArmando-TestCoder@users.noreply.github.com>
2021-02-16 21:49:47 -0600

    Take default out

--- src/utils/callScenes3D/index.tsx
+++ src/utils/callScenes3D/index.tsx
@@ -2,6 +2,6 @@ import * as scenes3D from '../../scenes'
 
 export default (sceneNames: string[], id: string) => {
     for (const sceneName of sceneNames) {
-        scenes3D[sceneName].default(id)
+        scenes3D[sceneName](id)
     }
 }
